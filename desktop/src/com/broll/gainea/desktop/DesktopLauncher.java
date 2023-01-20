7d8c85d5b750b9be59f79706c897a6f0b386b6f1
rolleander <roll2@web.de>
2023-01-20 23:47:26 +0100

    update gradle

--- desktop/src/com/broll/gainea/desktop/DesktopLauncher.java
+++ desktop/src/com/broll/gainea/desktop/DesktopLauncher.java
@@ -1,14 +1,8 @@
 package com.broll.gainea.desktop;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.broll.gainea.Gainea;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.net.MalformedURLException;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
